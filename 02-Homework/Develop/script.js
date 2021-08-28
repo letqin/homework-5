5c179f4b145c88bd3064c470fddc850daa2d1621
letqin <86627655+letqin@users.noreply.github.com>
2021-08-27 20:34:10 -0400

    changed some of the pseudo code in the script file

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -2,8 +2,8 @@
 
 // present all events to the user of the page that are within business hours
 
-// give the user the ability to add and delete events from the schedule
-
 // events are color coded depending on if the event has happened, is going to, or is already happening
+
+// give the user the ability to add and delete events from the selected time blocks
 
 // events are saved in the local storage so when the user returns, the data remains on the page and persists through a refresh of the page
