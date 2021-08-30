df940c68425f1f9e958088f80316f80be66f3fed
letqin <86627655+letqin@users.noreply.github.com>
2021-08-30 14:32:32 -0400

    reverted to original

--- Homework/Develop/script/script.js
+++
@@ -1,9 +0,0 @@
-// the current date is at the top of the page when the user loads it
-
-// present all events to the user of the page that are within business hours
-
-// events are color coded depending on if the event has happened, is going to, or is already happening
-
-// give the user the ability to add and delete events from the selected time blocks
-
-// events are saved in the local storage so when the user returns, the data remains on the page and persists through a refresh of the page
