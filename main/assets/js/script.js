b1c327300e0e7580c1eea37ce698d2af2036fb80
Quinn Ryan <letqin@gmail.com>
2022-01-21 15:13:30 -0500

    local storage is not happy

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,9 +1,9 @@
-let date = $("today");
+let date = $("#currentDay");
 let time = moment().format("dddd, MMMM Do");
-let save = $("save");
+let save = $(".saveBtn");
 const data = localStorage;
 
-$("today").text(moment().format("MM Do YYYY, hh:mm:ss a"));
+$("today").text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
 
 function getTime() {
     let hour = moment().hours();
@@ -12,7 +12,7 @@ function getTime() {
         let currentHour = parseInt($(this).attr("id"));
 
         if (currentHour > hour) {
-            $(this).addClass("upcoming");
+            $(this).addClass("future");
         } else if (currentHour === hour) {
             $(this).addClass("present");
         } else {
@@ -23,9 +23,20 @@ function getTime() {
 
 $(".saveBtn").click(function (event) {
     event.preventDefault();
-    let text = $(this).siblings(".time-block").val();
     let time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, text)
+    let value = $(this).siblings(".time-block").val();
+    localStorage.setItem(time, value)
 });
+
+$("#hour-08 .time-block").val(localStorage.getItem("08"));
+$("#hour-09 .time-block").val(localStorage.getItem("09"));
+$("#hour-10 .time-block").val(localStorage.getItem("10"));
+$("#hour-11 .time-block").val(localStorage.getItem("11"));
+$("#hour-12 .time-block").val(localStorage.getItem("12"));
+$("#hour-13 .time-block").val(localStorage.getItem("13"));
+$("#hour-14 .time-block").val(localStorage.getItem("14"));
+$("#hour-15 .time-block").val(localStorage.getItem("15"));
+$("#hour-16 .time-block").val(localStorage.getItem("16"));
+
 
 getTime();
