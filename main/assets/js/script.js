5a8ee93da9dad8433b63ba3e2f060f29c9c952cb
Quinn Ryan <letqin@gmail.com>
2022-01-23 17:05:32 -0500

    works now

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,42 +1,49 @@
-let date = $("#currentDay");
-let time = moment().format("dddd, MMMM Do");
-let save = $(".saveBtn");
-const data = localStorage;
+$(document).ready(function() {
 
-$("today").text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
+    $('#currentDay').text(moment().format("dddd, MMMM Do"))
+    $(".saveBtn").on('click', function(event) {
 
-function getTime() {
-    let hour = moment().hours();
-
-    $(".time-block").each(function () {
-        let currentHour = parseInt($(this).attr("id"));
-
-        if (currentHour > hour) {
-            $(this).addClass("future");
-        } else if (currentHour === hour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("past");
-        }
-    });
-};
-
-$(".saveBtn").click(function (event) {
     event.preventDefault();
-    let time = $(this).parent().attr("id").split("-")[1];
-    let value = $(this).siblings(".time-block").val();
-    localStorage.setItem(time, value)
-});
 
-$("#hour-08 .time-block").val(localStorage.getItem("08"));
-$("#hour-09 .time-block").val(localStorage.getItem("09"));
-$("#hour-10 .time-block").val(localStorage.getItem("10"));
-$("#hour-11 .time-block").val(localStorage.getItem("11"));
-$("#hour-12 .time-block").val(localStorage.getItem("12"));
-$("#hour-13 .time-block").val(localStorage.getItem("13"));
-$("#hour-14 .time-block").val(localStorage.getItem("14"));
-$("#hour-15 .time-block").val(localStorage.getItem("15"));
-$("#hour-16 .time-block").val(localStorage.getItem("16"));
+    var userText = $(this).siblings(".text").val();
+    var time = $(this).parent().attr('id');
 
+    localStorage.setItem(time, userText);
 
-getTime();
+    $('.notification').addClass("show")
+
+    setTimeout(function(){
+        $('.notification').removeClass("show")
+    }, 5000)
+    });
+
+    function keepCurrentTime() {
+        var currentHour = moment().hours();
+
+        $('.time-block').each(function () {
+            var timeBlockHour = parseInt($(this).attr('id').split('-')[1])
+            if(timeBlockHour < currentHour) {
+                $(this).addClass("past")
+            } else if (timeBlockHour === currentHour) {
+                $(this).removeClass("past")
+                $(this).addClass("present")
+            } else {
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
+            };
+        })
+    }
+
+    keepCurrentTime();
+
+    $("#hour-9 .text").val(localStorage.getItem('hour-9'))
+    $("#hour-10 .text").val(localStorage.getItem('hour-10'))
+    $("#hour-11 .text").val(localStorage.getItem('hour-11'))
+    $("#hour-12 .text").val(localStorage.getItem('hour-12'))
+    $("#hour-13 .text").val(localStorage.getItem('hour-13'))
+    $("#hour-14 .text").val(localStorage.getItem('hour-14'))
+    $("#hour-15 .text").val(localStorage.getItem('hour-15'))
+    $("#hour-16 .text").val(localStorage.getItem('hour-16'))
+    $("#hour-17 .text").val(localStorage.getItem('hour-17'))
+})
